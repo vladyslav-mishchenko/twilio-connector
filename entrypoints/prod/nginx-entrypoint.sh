8d6a05e06d068017febfad74d86bbccbacc8d89f
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2025-08-30 00:15:20 +0300

    refactor(nginx): use environment variable for SSL certificate paths

--- entrypoints/prod/nginx-entrypoint.sh
+++ entrypoints/prod/nginx-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 source /etc/nginx/nginx-env.sh
 
-envsubst '${NGINX_HOSTNAME}' \
+envsubst '${NGINX_HOSTNAME}' '${DOMAIN}' \
     < /etc/nginx/templates/default.conf.template \
     > /etc/nginx/conf.d/default.conf
 
